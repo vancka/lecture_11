5cd1d6832b955b793876f6ed09cac274336e1386
vancka <xvanco07@vutbr.cz>
2021-04-19 10:19:14 +0200

    hotova fce read_data

--- searching.py
+++ searching.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 # get current working directory path
 cwd_path = os.getcwd()
@@ -11,11 +12,20 @@ def read_data(file_name, field):
     :param field: (str), field of a dict to return
     :return: (list, string),
     """
+    if field not in {"unordered_numbers", "ordered_numbers", "dna_sequence"}:
+        return None
+
     file_path = os.path.join(cwd_path, file_name)
+
+    with open(file_path, "r") as json_file:
+        seq = json.load(json_file)
+
+    return seq[field]
 
 
 def main():
-    pass
+    seq = read_data("sequential.json", "unordered_numbers")
+    print(seq)
 
 
 if __name__ == '__main__':
